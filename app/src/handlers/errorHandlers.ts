47c7282806b408c7d2d8893b1adeb7c8216be874
Matthew <matthewbidewell@gmail.com>
2018-03-21 09:07:12 +0000

    added errorHandler functions

---
+++ app/src/handlers/errorHandlers.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express';
+
+
+class ErrorHandlers {
+
+  // If route not found, make a new error a pass it along.
+  public notFound = (req: Request, res: Response, next: NextFunction): void => {
+    const err = new Error();
+    err.message = 'Not found';
+    res.status(404);
+
+    next(err);
+  }
+
+  // send back error message and error status code.
+  public handleError = (err, req: Request, res: Response): void => {
+    if (process.env.NODE_ENV === 'PRODUCTION') {
+      res.json(err.message);
+    }
+    else {
+      const errorDetails = {
+        status: res.status(),
+        message: err.message,
+        stack: err.stack || '',
+      };
+      res.json(errorDetails);
+    }
+  }
+}
+
+const errorHandlers = new ErrorHandlers();
+
+export default errorHandlers;
